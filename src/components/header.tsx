eeb766e45709cf702649c74a083e25440b5c4438
tu801 <sloveoflove@gmail.com>
2025-01-31 16:52:25 +0700

    complete search page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,17 +7,38 @@ import {
   useThemeMode,
 } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { SignedIn, SignedOut, UserButton, useUser } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import Image from "next/image";
 import AdminMenu from "./template/admin-menu";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 export default function Headers() {
   const { mode } = useThemeMode();
   const path = usePathname();
   const { isSignedIn, user } = useUser();
+  const router = useRouter();
+  const [searchTerms, setSearchTerms] = useState("");
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(searchParams);
+    const searchParamsFromUrl = urlParams.get("searchTerms");
+    if (searchParamsFromUrl) {
+      setSearchTerms(searchParamsFromUrl);
+    }
+  }, [searchParams]);
+
+  const handleSubmit = () => (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(searchParams);
+    urlParams.set("searchTerms", searchTerms);
+    const searchQuery = urlParams.toString();
+    router.push(`/search?${searchQuery}`);
+  };
 
   return (
     <Navbar fluid rounded>
@@ -30,11 +51,13 @@ export default function Headers() {
           alt="Flowbite React Logo"
         />
       </Navbar.Brand>
-      <form>
+      <form onSubmit={handleSubmit()}>
         <TextInput
           type="text"
           placeholder="Search"
           rightIcon={AiOutlineSearch}
+          value={searchTerms}
+          onChange={(e) => setSearchTerms(e.target.value)}
         />
       </form>
       <div className="flex gap-2 md:order-2">
